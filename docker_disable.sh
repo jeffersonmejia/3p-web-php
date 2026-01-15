453c8ebb756cc9a755bbab74da67d8f9c540fa7e
Jefferson Mejia <jefffersonmejiach01@gmail.com>
2026-01-15 09:33:42 -0500

    refactor: update script docker file to enable and disable

---
+++ docker_disable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#!/bin/bash
+
+set -e
+
+sudo systemctl disable --now containerd
+sudo systemctl disable --now docker.service
+sudo systemctl disable --now docker.socket
+
+
+sudo systemctl status docker.service --no-pager
---
+++ docker_enable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#!/bin/bash
+
+set -e
+
+sudo systemctl enable --now containerd
+sudo systemctl enable --now docker.service
+sudo systemctl enable --now docker.socket
+
+sudo systemctl status docker.service --no-pager
