94e704ccb3ad8fd09e6980f828c527911ecf2d21
Jefferson Mejia <jefffersonmejiach01@gmail.com>
2026-01-22 09:48:16 -0500

    feat: add posts at home

--- src/app/controller/home.php
+++ src/app/controller/home.php
@@ -1,3 +1,17 @@
 <?php
 $title = 'Home';
+
+$dsn = "mysql:host=db;dbname=web_php;charset=utf8mb4";
+
+try {
+	$pdo = new PDO($dsn, 'db', 'db', [
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+	]);
+
+	$posts = $pdo->query("SELECT * FROM posts ORDER BY id DESC LIMIT 6")
+		->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+	die("Error de conexión: " . $e->getMessage());
+}
+
 require __DIR__ . '/../../resources/home.template.php';
