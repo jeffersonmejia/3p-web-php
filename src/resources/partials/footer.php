54fe71a1a7b4c6a72c5b65f5483902c16af2bea6
Jefferson Mejia <jefffersonmejiach01@gmail.com>
2026-01-22 09:38:38 -0500

    style: update footer

--- src/resources/partials/footer.php
+++ src/resources/partials/footer.php
@@ -1,7 +1,9 @@
-	</div>
-	<footer>
-		<p>Todos los derechos reservados <?= date('Y') ?> </p>
-	</footer>
-	</body>
+</div>
 
-	</html>
+<footer class="w-full border-t border-gray-200 py-4 text-center text-sm text-gray-500">
+	<p>Todos los derechos reservados <?= date('Y') ?></p>
+</footer>
+
+</body>
+
+</html>
