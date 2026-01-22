9a09e77824795cbf0dfd1ef666d18f293a06c1c9
Jefferson Mejia <jefffersonmejiach01@gmail.com>
2026-01-22 10:16:56 -0500

    refactor: delete unused folders db

--- src/app/controller/links.php
+++ src/app/controller/links.php
@@ -2,16 +2,10 @@
 $title = 'Projects';
 
 $dsn = "mysql:host=db;dbname=web_php;charset=utf8mb4";
-
-try {
-	$pdo = new PDO($dsn, 'db', 'db', [
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-	]);
-
-	$links = $pdo->query("SELECT * FROM links")
-		->fetchAll(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-	die("Error de conexión: " . $e->getMessage());
-}
+$pdo = new PDO($dsn, 'db', 'db', [
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+$links = $pdo->query("SELECT * FROM links")
+	->fetchAll(PDO::FETCH_ASSOC);
 
 require __DIR__ . '/../../resources/links.template.php';
