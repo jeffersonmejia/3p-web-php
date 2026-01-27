d9f508d9cfd1fb199eb505d4cf1ffa939de63f84
Jefferson Mejia <jefffersonmejiach01@gmail.com>
2026-01-27 09:31:58 -0500

    refactor: update docroot to public

--- public/index.php
+++ public/index.php
@@ -3,12 +3,12 @@ require __DIR__ . "/../framework/Database.php";
 
 $db = new Database();
 
-$routes = require __DIR__ . '/routes/web.php';
+$routes = require __DIR__ . '/../routes/web.php';
 $requestUri = $_SERVER['REQUEST_URI'];
 $route = $routes[$requestUri] ?? null;
 
 if ($route) {
-	require __DIR__ . '/' . $route;
+	require __DIR__ . '/../' . $route;
 } else {
 	http_response_code(404);
 	echo "404 not found";
