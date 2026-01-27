58978538bf0bbae98c7ca6be3fa7880c5affe3dd
Jefferson Mejia <jefffersonmejiach01@gmail.com>
2026-01-27 10:19:51 -0500

    feature: add secure params in

--- public/index.php
+++ public/index.php
@@ -4,9 +4,11 @@ require __DIR__ . "/../framework/Database.php";
 $db = new Database();
 
 $routes = require __DIR__ . '/../routes/web.php';
-$requestUri = $_SERVER['REQUEST_URI'];
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $route = $routes[$requestUri] ?? null;
 
+/*echo var_dump($requestUri);
+die();*/
 if ($route) {
 	require __DIR__ . '/../' . $route;
 } else {
